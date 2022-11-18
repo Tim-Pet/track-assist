d5be5b5fcdf3b7370e9678f3e60a625043fbf240
Tim Petersen <tim.petersen1@outlook.de>
2022-11-18 13:32:49 +0100

    chore(setup): :wrench: add craco bundling & chrome content script setup

---
+++ src/chromeServices/contentScript.example.ts
@@ -0,0 +1,4 @@
+export {};
+// This is an example file for the content script. It is not used in the extension.
+// All content scripts belong in this folder
+// A contentScript typically doesn't export anything, but rather add event listeners to the window/chrome object
